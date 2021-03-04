15aef3790cd9cd49d9128e891302ea388a76def3
bgcrafts <bartosz.giecewicz@gmail.com>
2021-03-04 13:17:52 +0100

    dom04_obsługa formularza

---
+++ zadania-domowe/dom/dom04_obsługa formularza/js/main.js
@@ -0,0 +1,12 @@
+console.log('Warsztat - obsługa formularza"Imię i nazwisko" ');
+
+let form = document.getElementById('form');
+
+const submitForm = (event) => {
+    event.preventDefault();
+    let firstName = document.querySelector('[name="fname"]');
+    let lastName = document.querySelector('[name="lname"]');
+    console.log(`Imie: ${firstName.value}, Nazwisko: ${lastName.value}`);
+}
+
+form.addEventListener('submit', submitForm);
